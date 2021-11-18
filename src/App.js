156cfa4a31d71810fe201f919e94c8ac071468a6
Pavel Hladki <->
2021-11-18 14:56:57 +0300

    feat: add footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import './styles/global.scss';
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
