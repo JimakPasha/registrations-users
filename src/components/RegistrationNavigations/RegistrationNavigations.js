e3bfb5ae8883327f07e12f5b0ea813bef3441be8
jimak <jiamak-pg@yandex.by>
2021-11-30 11:30:49 +0300

    feat: add active tabs

--- src/components/RegistrationNavigations/RegistrationNavigations.js
+++ src/components/RegistrationNavigations/RegistrationNavigations.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './RegistrationNavigations.scss';
 
 const RegistrationNavigations = () => {
@@ -9,11 +10,36 @@ const RegistrationNavigations = () => {
           <h2 className="forms-wrap__title">Типовая форма</h2>
           <nav className="forms-wrap__navigations">
             <ul className="forms-wrap__list">
-              <li className="forms-wrap__item forms-wrap__item--active">
-                Личные данные
+              <li className="forms-wrap__item">
+                <NavLink
+                  className="forms-wrap__link"
+                  activeClassName="forms-wrap__link--active"
+                  exact
+                  to="/registration/personal/"
+                >
+                  Личные данные
+                </NavLink>
               </li>
-              <li className="forms-wrap__item">Данные кредитной карты</li>
-              <li className="forms-wrap__item">Результат</li>
+              <li className="forms-wrap__item">
+                <NavLink
+                  className="forms-wrap__link"
+                  activeClassName="forms-wrap__link--active"
+                  exact
+                  to="/registration/card/"
+                >
+                  Данные кредитной карты
+                </NavLink>
+              </li>
+              <li className="forms-wrap__item">
+                <NavLink
+                  className="forms-wrap__link"
+                  activeClassName="forms-wrap__link--active"
+                  exact
+                  to="/registration/result/"
+                >
+                  Результат
+                </NavLink>
+              </li>
             </ul>
           </nav>
         </div>
